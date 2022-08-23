4f95853cbd4893b27531908e9376c70a322e58ee
Grossevich Lev <levgross@gmail.com>
2022-08-23 17:48:12 +0400

    3. Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+       try {
+           char[] chars = {'\\', '|', '/'};
+           int i = 1;
+           while (!Thread.currentThread().isInterrupted()) {
+               System.out.print("\r loading ... " + chars[i]);
+               Thread.sleep(500);
+               i = i == chars.length - 1 ? 0 : i + 1;
+           }
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
