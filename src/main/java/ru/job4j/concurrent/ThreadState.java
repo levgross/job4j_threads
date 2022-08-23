68d06a421d0e720faf4b3a006345394667594bb5
Grossevich Lev <levgross@gmail.com>
2022-08-23 22:03:07 +0400

    1.1. Состояние нити.

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -4,10 +4,14 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> { }
+                () -> {
+                    System.out.println("First thread");
+                }
         );
         Thread second = new Thread(
-                () -> { }
+                () -> {
+                    System.out.println("Second thread");
+                }
         );
         System.out.println(first.getName() + " " + first.getState());
         System.out.println(second.getName() + " " + second.getState());
