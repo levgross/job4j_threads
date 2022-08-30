3a2b0494a1db225c6392f9fcf76de4a341916239
Grossevich Lev <levgross@gmail.com>
2022-08-30 23:23:28 +0400

    1. Реализовать шаблон Producer Consumer.

--- src/test/java/ru/job4j/buffer/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/buffer/SimpleBlockingQueueTest.java
@@ -6,12 +6,24 @@ import ru.job4j.buffer.SimpleBlockingQueue;
 class SimpleBlockingQueueTest {
     @Test
     public void whenOfferThenPoll() throws InterruptedException {
-//        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-//        Thread producer = new Thread(() -> queue.offer(1));
-////        Thread consumer = new Thread(queue::poll);
-//        producer.start();
-//        consumer.start();
-//        producer.join();
-//        consumer.join();
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(() -> {
+            try {
+                queue.offer(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            try {
+                queue.poll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
     }
 }
