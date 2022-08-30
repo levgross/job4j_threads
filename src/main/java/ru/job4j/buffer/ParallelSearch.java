6922adef64bacbbcaeb082bd0473a1024db18cc2
Grossevich Lev <levgross@gmail.com>
2022-08-30 23:26:00 +0400

    2. Обеспечить остановку потребителя

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,38 @@
+package ru.job4j.buffer;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int i = 0; i != 3; i++) {
+                        try {
+                            queue.offer(i);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+
+                }
+        ).start();
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        consumer.interrupt();
+    }
+}
